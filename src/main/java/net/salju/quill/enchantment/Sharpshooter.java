361c7a874a4da99437e8a398481303de38f9a375
Salju <61861078+Jusey1@users.noreply.github.com>
2024-08-29 00:39:16 -0400

    Delete src/main/java/net/salju/quill/enchantment/Sharpshooter.java

--- src/main/java/net/salju/quill/enchantment/Sharpshooter.java
+++
@@ -1,24 +0,0 @@
-package net.salju.quill.enchantment;
-
-import net.salju.quill.init.QuillEnchantments;
-import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.entity.EquipmentSlot;
-import java.util.List;
-
-public class Sharpshooter extends Enchantment {
-	public Sharpshooter(EquipmentSlot... slots) {
-		super(Enchantment.Rarity.COMMON, EnchantmentCategory.CROSSBOW, slots);
-	}
-
-	@Override
-	public int getMaxLevel() {
-		return 5;
-	}
-
-	@Override
-	protected boolean checkCompatibility(Enchantment ench) {
-		return List.of(Enchantments.MULTISHOT, Enchantments.PIERCING).contains(ench) ? false : super.checkCompatibility(ench);
-	}
-}
