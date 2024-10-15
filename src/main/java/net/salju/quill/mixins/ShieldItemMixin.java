4fb570df5240affb0e89d8936fd0b7cf10c85afb
Salju <61861078+Jusey1@users.noreply.github.com>
2024-10-15 19:57:29 -0400

    Deleting Files

--- src/main/java/net/salju/quill/mixins/ShieldItemMixin.java
+++
@@ -1,19 +0,0 @@
-package net.salju.quill.mixins;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.salju.quill.init.QuillConfig;
-import net.minecraft.world.item.ShieldItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Equipable;
-
-@Mixin(ShieldItem.class)
-public abstract class ShieldItemMixin extends Item implements Equipable {
-	public ShieldItemMixin(Item.Properties props) {
-		super(props);
-	}
-
-	@Override
-	public int getEnchantmentValue() {
-		return (QuillConfig.ENCHS.get() ? 5 : super.getEnchantmentValue());
-	}
-}
